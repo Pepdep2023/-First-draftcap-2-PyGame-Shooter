ae3b3a77b16ae32690dab72843c1c3a21a245314
Pepdep2023 <03230228.gcbs@rub.edu.bt>
2023-10-17 10:02:13 +0600

    Add to GitHub

--- Day 1.py
+++ Day 1.py
@@ -3,3 +3,9 @@ import pygame
 pygame.init()
 #creating screen for the game
 screen = pygame.display.set_mode((1000,800))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
