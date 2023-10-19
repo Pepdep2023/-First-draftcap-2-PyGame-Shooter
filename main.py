ef2b010b577b37d514d73065e0e487345d636516
my_GitHub <03230228.gcbs@rub.edu.bt>
2023-10-19 13:57:16 +0600

    screen color

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ pygame.init()
 #creating screen for the game
 screen = pygame.display.set_mode((1000,800))
 
+#Title and Icon
+pygame.display.set_caption("SHOOTER")
+icon = pygame.image.load('ufo (2).png')
+pygame.display.set_icon(icon)
+
+#Player
+player_image = pygame.image.load('spaceship.png')
+playerX = 500
+playerY = 650
+
+def player():
+    screen.blit(player_image,(playerX,playerY))
 
 #Game loop(window doesn't close down or hanging)
 running = True
@@ -11,4 +23,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+# Screen colour:RBG(Red,Blue and Green)
+        screen.fill((128,128,0))
+ #write 'player()' after screen.fill
+        player()
+        pygame.display.update()
 
