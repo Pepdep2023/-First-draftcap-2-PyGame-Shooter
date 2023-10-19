ac743a56547b1d76d36d2c75cceaccd7ec51d459
my_GitHub <03230228.gcbs@rub.edu.bt>
2023-10-19 11:12:43 +0600

    Title and Icon

--- Day 1.py
+++ Day 1.py
@@ -3,6 +3,8 @@ import pygame
 pygame.init()
 #creating screen for the game
 screen = pygame.display.set_mode((1000,800))
+
+
 #Game loop(window doesn't close down or hanging)
 running = True
 while running:
